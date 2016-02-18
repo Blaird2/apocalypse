3f624c3b3235dbac73a7bbde8c96410f31988ef0
Hilmi A <2ndgenairborne@gmail.com>
2016-02-18 13:39:01 -0700

    Added greedy AI
    AI still does not work, but added some helper functions.

--- ApocStrategyGreedy.hs
+++ ApocStrategyGreedy.hs
@@ -20,67 +20,64 @@ import Data.Maybe (fromJust, isNothing)
 import System.IO.Unsafe
 import ApocTools
 
-{- | This is just a placeholder for the greedy strategy: it always chooses to play
-     (0,0) to (2,1).
--}
+
+
 --greedy  :: GameState -> PlayType -> Player -> IO (Maybe [(Int,Int)])
 greedy    :: Chooser
-greedy board Normal player = greedyNormal board player
-greedy board PawnPlacement player = greedyPawnPlacement board player
+greedy gamestate Normal player = greedyNormal (theBoard gamestate) player
+greedy gamestate PawnPlacement player = greedyPawnPlacement (theBoard gamestate) player
 
-greedyNormal :: board -> player -> IO (Maybe [(Int,Int)])
+greedyNormal :: Board -> Player -> IO (Maybe [(Int,Int)])
 greedyNormal _ _ = return (Just[(0,0), (2,1)])
 
-greedyPawnPlacement :: board -> player -> IO (Maybe [(Int,Int)])
+greedyPawnPlacement :: Board -> Player -> IO (Maybe [(Int,Int)])
 greedyPawnPlacement _ _ = return (Just [(2,2)])
 
--- | Given the coordinates of a pawn, generates a list of tuples containing every possible move that pawn can legally make.
-pawnValidMoves :: (Int, Int) -> board -> player -> [(Int),(Int)]
-pawnValidMoves (srcX, srcY) board player = if (getFromBoard board (srcX, srcY + forwards)) == E)
-                                          then
-                      () = [(srcX, srcY +forwards)]
-                | (getFromBoard board (srcX, srcY + forwards)) != Empty) = [()]
-                where forwards = case of player of
-                  Black -> -1
-                  White -> 1
-
-checkValidMoves :: Int -> [(Int, Int)] -> (Int, Int) -> Board -> Player -> [(Int, Int)]
-checkValidMoves 0 xs (srcX, srcY) board player
-                                                | getFromBoard board (srcX, srcY + forwards) == E = checkValidMoves counter+1 xs:[(srcX, srcY + forwards)]  board player
-                                                | otherwise checkValidMoves counter+1 xs board player
-                                                where forwards = case of player of
-                                                  Black -> -1
-                                                  White -> 1
-
-checkValidMoves 1 xs (srcX, srcY) board player    | getFromBoard board (srcX+1 , srcY + forwards) && playerOf pieceTo /= player = checkValidMoves counter+1 xs:[(srcX, srcY + forwards)]  board player
-                                                  | otherwise checkValidMoves counter+1 xs board player
-                                                  where forwards = case of player of
-                                                          Black -> -1
-                                                          White -> 1
-                                                        pieceTo = pieceOf $ getFromBoard board (srX, srcY)
+-- | These two statements allow for ease of checking valid moves.
+pawnPossibleMoves = [(0,1), (1,1), (1,-1)]
+pawnExpectedPiece  = ["E", "NE", "NE"]
+knightPossibleMoves = [(1,2), (1,-2), (-1,2), (-1,-2), (2,1), (2,-1), (-2,1), (-2,-1)]
 
 
-checkValidMoves 2 xs (srcX, srcY) board player = | getFromBoard board (srcX, srcY + forwards) == E = checkValidMoves counter+1 xs:[(srcX, srcY + forwards)]  board player
-| otherwise checkValidMoves counter+1 xs board player
-where forwards = case of player of
-  Black -> -1
-  White -> 1
 
-{-}-- | Determines the validity of a pawn movement
-isPawnMoveValid :: Board -> Player -> (Int, Int) -> (Int, Int) -> Bool
-isPawnMoveValid board player (fromX, fromY) to
-       | to == (fromX, fromY + forwards) = True
-       | to == (fromX + 1, fromY + forwards) && playerOf pieceTo /= player = True
-       | to == (fromX - 1, fromY + forwards) && playerOf pieceTo /= player = True
-       | otherwise = False
-       where forwards = case player of
-               Black -> -1
-               White -> 1
-             pieceTo = pieceOf $ getFromBoard board to-}
+pawnMoveChecker :: [(Int, Int)] -> [String] -> (Int, Int) -> Board -> Player -> [(Int, Int)]
+pawnMoveChecker [] _ _ _ _ = []
+pawnMoveChecker ((posX, posY):xs) (e:es) (srcX, srcY) board player = if (player == Black)
+                                                              then  if    (getFromBoard board (srcX+posX, srcY+posY)==(fst (pawnEmptytoPiece e player)) ||  getFromBoard board (srcX+posX, srcY+posY)==(snd (pawnEmptytoPiece e player)))
+                                                                then  (srcX+posX, srcY+posY):(pawnMoveChecker xs es (srcX, srcY) board player)
+                                                                else  pawnMoveChecker xs es (srcX, srcY) board player
+                                                              else  if    (getFromBoard board (srcX-posX, srcY-posY)==(fst (pawnEmptytoPiece e player)) ||  getFromBoard board (srcX-posX, srcY-posY)==(snd (pawnEmptytoPiece e player)))
+                                                                then  (srcX-posX, srcY-posY):(pawnMoveChecker xs es (srcX, srcY) board player)
+                                                                else  pawnMoveChecker xs es (srcX, srcY) board player
 
 
--- | Given the coordinates of a knight, generates a list of tuples containing every possible move that knight can legally make.
-knightValidMoves :: (Int, Int) -> board -> [(Int), (Int)]
+knightMoveChecker :: [(Int, Int)] -> (Int, Int) -> Board -> Player -> [(Int, Int)]
+knightMoveChecker [] _ _ _ = []
+knightMoveChecker ((posX, posY):xs) (srcX, srcY) board player = if (player == Black)
+                                                                then  if (getFromBoard board (srcX+posX, srcY+posY)/=(BK) || getFromBoard board (srcX+posX, srcY+posY)/=(BP))
+                                                                      then (srcX+posX, srcY+posY):(knightMoveChecker xs (srcX, srcY) board player)
+                                                                      else knightMoveChecker xs (srcX, srcY) board player
+                                                                else  if (getFromBoard board (srcX-posX, srcY-posY)/=(WK) || getFromBoard board (srcX-posX, srcY-posY)/=(WP))
+                                                                      then (srcX-posX, srcY-posY):(knightMoveChecker xs (srcX, srcY) board player)
+                                                                      else knightMoveChecker xs (srcX, srcY) board player
+
+
+{-
+-- | validMoveChecker:: Every possible move the pawn/knight can make -> List of expected returns values -> Current Location -> the Board -> Player -> Returns list of possible moves this pawn can make
+validMoveChecker :: [(Int, Int)] -> [String]-> (Int, Int)-> Board -> Player -> [(Int, Int)]
+validMoveChecker [] _ _ _ _ = []
+validMoveChecker ((posX, posY):xs) (curCell:cells) (srcX, srcY) board player =  if (player == Black)
+                                                                then  if    (getFromBoard board (srcX+posX, srcY+posY)==)
+                                                                      then  (srcX+posX, srcY+posY):(validMoveChecker xs cells (srcX, srcY) board player)
+                                                                      else  validMoveChecker xs cells (srcX, srcY) board player
+                                                                else  if    (getFromBoard board (srcX-posX, srcY-posY)==curCell)
+                                                                      then  (srcX-posX, srcY-posY):(validMoveChecker xs cells (srcX, srcY) board player)
+                                                                      else  validMoveChecker xs cells (srcX, srcY) board player
+-}
+pawnEmptytoPiece :: String -> Player -> (Cell, Cell)
+pawnEmptytoPiece "E" _ = (E, E)
+pawnEmptytoPiece "NE" White = (BK, BP)
+pawnEmptytoPiece "NE" Black = (WK, WP)
 
 
 -- | Checks to see if an integer is within the board range
